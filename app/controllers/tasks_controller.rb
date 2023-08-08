1d2aeadd65cb86037fa8b7b2098fba6869a13c1e
Mana Misawa <mnmisawa04@gmail.com>
2023-08-08 15:59:17 +0900

    Done the challenge(need styling)

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+# comment
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update delete]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Show all the tasks
+  get '/tasks', to: 'tasks#index', as: :tasks
+
+  # Add a task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  # Show the detail of one specific task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # Edit the task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # Delete the task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
